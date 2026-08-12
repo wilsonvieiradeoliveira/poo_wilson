98bf7f20345a576d45fff39ce1a039b1df8a022f
wilson <wilson22vieira@gmail.com>
2026-08-12 19:02:33 -0300

    alteração para resolução do problema

--- ex7_jogo.py
+++ ex7_jogo.py
@@ -1,15 +1,21 @@
 segredo = 7
 tentativas = 0
+max_tentativas = 5
+acertou = False
 
-palpite = int(input("Tente adivinhar (1-10): "))
-tentativas += 1
-
-while palpite != segredo:
-    if palpite > segredo:
-        print("Menor!")
-    else:
-        print("Maior!")
-    palpite = int(input("Tente de novo: "))
+while tentativas < max_tentativas:
+    palpite = int(input("Tente adivinhar (1-10): "))
     tentativas += 1
+    if palpite == segredo:
+        acertou = True
+        break
+    elif palpite > segredo:
+        print("O segredo é menor!")
+    else:
+        print("O segredo é maior!")
 
-print(f"Parabéns! Você acertou em {tentativas} tentativas!")
+if acertou:
+    print(f"Parabéns! Você acertou em {tentativas} tentativas!")
+else:
+    print(f"Gamer Over! O número secreto era {segredo}")
+
