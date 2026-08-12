8d787e9492ece6c38741effb6ae78c53f8a7f0c0
wilson <wilson22vieira@gmail.com>
2026-08-12 18:37:42 -0300

    criação do mini-desafio calculadora de tempo de tela

---
+++ mini-desafio_calc-de-tempo-de-tela.py
@@ -0,0 +1,6 @@
+horas_dia = float(input("Quantas horas por dia você usa o celular? "))
+horas_semana = horas_dia * 7
+horas_ano = horas_dia * 365
+
+print(f"Por semana: {horas_semana} horas")
+print(f"Por ano: {horas_ano} horas")
