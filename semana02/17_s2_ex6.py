f523971bdfeff930a6ffc2d5fbe36351e74f3cdc
wilson <wilson22vieira@gmail.com>
2026-08-19 14:05:22 -0300

    adiciona exercicio: dicionario com criacao e leitura

---
+++ semana02/17_s2_ex6.py
@@ -0,0 +1,8 @@
+perfil = {
+    "usuario": "ana_dev",
+    "seguidores": 320,
+    "bio": "Apaixonada por tecnologia",
+}
+
+print(f"@{perfil['usuario']} - {perfil['seguidores']} seguidores")
+print(perfil["bio"])
