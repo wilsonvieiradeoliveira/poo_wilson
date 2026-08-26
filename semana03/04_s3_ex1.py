b52d55520cb00f6b669e9ac1e81ed554839304f9
wilson <wilson22vieira@gmail.com>
2026-08-26 18:17:38 -0300

    adiciona exercicio: Jogo

---
+++ semana03/04_s3_ex1.py
@@ -0,0 +1,13 @@
+class Jogo:
+    pass
+ 
+j1 = Jogo()
+j1.nome = "Minecraft"
+j1.nota = 9.5
+ 
+j2 = Jogo()
+j2.nome = "Valorant"
+j2.nota = 8.0
+ 
+print(f"{j1.nome} - nota {j1.nota}")
+print(f"{j2.nome} - nota {j2.nota}")
